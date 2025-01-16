d787eab8a16d82e84e6520c52989f1d5ad9b376d
kyanch <me@kyanch.icu>
2025-01-16 22:26:14 +0800

    change iso keymap

--- keyboards/phdesign/ph60/iso/keymaps/default/keymap.c
+++ keyboards/phdesign/ph60/iso/keymaps/default/keymap.c
@@ -13,19 +13,19 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[BASE] = LAYOUT_60_ansi(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, 
+[BASE] = LAYOUT_60_iso(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
-        KC_LSFT, MO(FN),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL
+        KC_LSFT, MO(FN), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(FN), KC_LEFT, KC_UP,   KC_RIGHT
 ),
-[FN] = LAYOUT_60_ansi(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_CALC, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP, KC_PSCR, _______, KC_PAUS,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, CUSTOM_1,CUSTOM_2,_______, _______, _______, KC_END,  KC_PGDN, _______,          _______,
-        _______, _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,          _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, _______
+[FN] = LAYOUT_60_iso(
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_CALC, _______, _______, _______, KC_INS,  KC_HOME, KC_PGUP, KC_PSCR, _______,
+        _______, KC_VOLD, KC_VOLU, KC_MUTE, CUSTOM_1,CUSTOM_2,KC_LEFT, KC_DOWN, KC_UP , KC_RIGHT,  KC_PGDN, _______, KC_PAUS, _______,
+        _______, KC_LSFT, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______,          _______,
+        _______, _______, _______,                            _______,                            _______, KC_LEFT, KC_DOWN, KC_RIGHT
 )
 };
 
@@ -46,8 +46,8 @@ led_config_t g_led_config = { {
     }, {
     // LED索引到标志的映射
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,    4,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,    4,
     4, 4, 4,          4,          4, 4, 4, 4
 } };
